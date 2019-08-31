980d09814c072739e80a225dd0560f48f975ede2
naelsondouglas <naelson17@gmail.com>
2019-08-30 21:14:28 -0300

    Added the code to plot the frequency histograms

--- src/filter_results.py
+++ src/filter_results.py
@@ -2,6 +2,9 @@ import json
 import os
 from pprint import pprint
 from collections import Counter
+import matplotlib.pyplot as plt
+
+tags_parent_dir="/home/ndc/repos/TCC/src/contracts/2015 - 204 arquivos"
 
 
 #Gets a directory and returns all the entities jsons on the directory subfolders
@@ -24,11 +27,30 @@ def extract_categ(jsons,category):
         data.append(j[category])
     return data
 
-j=get_jsons("/home/ndc/repos/TCC/src/contracts/2015 - 204 arquivos")
+
 
 def merge_jsons(jsons):
     merged = {}
     for j in jsons:
-        dict(Counter(j)+Counter(j))
+        merged = dict(Counter(j)+Counter(merged))
     return merged
 
+def dict_to_array(d,min_freq=5):
+    array = []
+    for k in d.keys():
+        for freq in range(0,d[k]):
+            if d[k] >= min_freq:
+                array.append(k)
+    return array
+
+def plot_entityFreq(directory=tags_parent_dir,entity_class = "PESSOA",min_freq=5):
+    j=get_jsons(directory)
+    j=extract_categ(j,entity_class)
+    j=merge_jsons(j)
+    j=dict_to_array(j,min_freq)
+    j = sorted(j)
+    plt.hist(j, histtype='bar', ec='black')
+    plt.ylabel('Frequência',fontsize=20)
+    plt.xticks(rotation=90)
+    plt.gcf().subplots_adjust(bottom=0.3)
+    plt.show()
